d5cb9b05277b48820ead9a803605650747395219
Olivia Whitley <oliviawhitley@MacBook-Pro-7.local>
2026-03-25 12:31:23 -0400

    Created Article Database

---
+++ intellij/src/main/java/ArticleDatabase.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ArticleDatabase {
+    private ArrayList<Article> articleList = new ArrayList<>();
+    private ArrayList<Integer> articleIds = new ArrayList<>();
+
+
+    public void saveArticles(ArrayList<Article> articles){}
+}
