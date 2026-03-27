642a7b38b584eabf8b75755b3854bdf5e792286c
Rae Justine Gedangoni <rgedangoni@bpascal.cs.vassar.edu>
2026-03-27 00:48:56 -0400

    Updated Class Diagram

--- intellij/src/main/java/Main.java
+++ intellij/src/main/java/Main.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        ArticleRetriever retriever = new ArticleRetriever();
+        ArrayList<Article> articles = retriever.articleList;
 
+        for (Article article : articles) {
+            System.out.println(article.getSummary());
+            System.out.println("---------------------------------------------------");
+        }
     }
 }
