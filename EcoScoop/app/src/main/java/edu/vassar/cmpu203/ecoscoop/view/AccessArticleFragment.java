53e7a7890475c1b9ee29fec0f8a96906f307843d
Rae <raegedangoni@gmail.com>
2026-04-13 16:00:36 -0400

    Updated Access Article

--- EcoScoop/app/src/main/java/edu/vassar/cmpu203/ecoscoop/view/AccessArticleFragment.java
+++ EcoScoop/app/src/main/java/edu/vassar/cmpu203/ecoscoop/view/AccessArticleFragment.java
@@ -8,16 +8,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.vassar.cmpu203.ecoscoop.databinding.ExampleArticleBinding;
 import edu.vassar.cmpu203.ecoscoop.databinding.FragmentAccessArticleBinding;
 
-public class AccessArticleFragment extends Fragment {
-    private FragmentAccessArticleBinding binding;
+public class AccessArticleFragment extends Fragment implements AccessArticleUI {
+    private ExampleArticleBinding binding;
+    private Listener listener;
 
-    // TODO: Rename and change types of parameters
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        this.binding = FragmentAccessArticleBinding.inflate(inflater);
+        this.binding = ExampleArticleBinding.inflate(inflater);
         return this.binding.getRoot();
 
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        binding.feedCard.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (listener != null) listener.onDisplayArticle();
+
+            }
+        });
+
+    }
+
+
+    public void onClick(View view){
+
+    }
+
+    @Override
+    public void setListener(Listener listener) { this.listener = listener;}
+
+    @Override
+    public void runDisplayArticle() {
+
+    }
+
+
 }
