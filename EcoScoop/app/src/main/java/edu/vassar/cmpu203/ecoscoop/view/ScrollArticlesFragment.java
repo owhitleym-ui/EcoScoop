df564199cd7ec76b75b441839b12f262e7a23b26
Olivia Whitley <oliviawhitley@MacBook-Pro-7.local>
2026-04-13 16:00:58 -0400

    Added ScrollArticlesFragment

---
+++ EcoScoop/app/src/main/java/edu/vassar/cmpu203/ecoscoop/view/ScrollArticlesFragment.java
@@ -0,0 +1,26 @@
+package edu.vassar.cmpu203.ecoscoop.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import edu.vassar.cmpu203.ecoscoop.databinding.FragmentAccessArticleBinding;
+import edu.vassar.cmpu203.ecoscoop.databinding.FragmentScrollArticlesBinding;
+
+class ScrollArticlesFragment extends Fragment {
+
+    private FragmentScrollArticlesBinding binding;
+
+    // TODO: Rename and change types of parameters
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.binding = FragmentScrollArticlesBinding.inflate(inflater);
+        return this.binding.getRoot();
+
+    }
+}
